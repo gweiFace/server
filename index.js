20a964b51b418f4ed455581a90f8e95a82dfa25c
prathik <37804760+prmali@users.noreply.github.com>
2021-03-08 12:20:24 -0600

    collection description

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.get("/token/:tokenId", (req, res) => {
 
 app.get("/contract", (req, res) => {
 	return res.status(200).send({
-		description: "",
+		description:
+			"gweiFace is a collection of 9,999 unique pieces of NFT art. gweiFaces are created through a random, unique combination of five characteristics: face, hair, shirt, accessory, and background. The rarities of these five characteristics vary, so look out for the rare gweiFaces! You can learn more about the project on our website (gweiface.com) and you can stay up to date on our project by following our twitter or joining our discord.",
 		external_link: "https://www.gweiface.com/",
 		image: "https://storage.googleapis.com/gwei-faces/logo.png",
 		name: "gweiFaces",
