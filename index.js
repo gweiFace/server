d85a882ee874a27af07721498154040cdd757fb1
prathik <37804760+prmali@users.noreply.github.com>
2021-03-15 17:24:55 -0500

    link in desc

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get("/factory/:factoryId", (req, res) => {
 app.get("/contract", (req, res) => {
 	return res.status(200).send({
 		description:
-			"gweiFace is a collection of 9,999 unique pieces of NFT art. gweiFaces are created through a random, unique combination of five characteristics: face, hair, shirt, accessory, and background. The rarities of these five characteristics vary, so look out for the rare gweiFaces! You can learn more about the project on our website (gweiface.com) and you can stay up to date on our project by following our twitter or joining our discord.",
+			"gweiFace is a collection of 9,999 unique pieces of NFT art. gweiFaces are created through a random, unique combination of five characteristics: face, hair, shirt, accessory, and background. The rarities of these five characteristics vary, so look out for the rare gweiFaces! You can learn more about the project on our website (gweiface.com) and you can stay up to date on our project by following our Twitter (@gweiFaceNFT) or joining our discord (discord.com/invite/T5yTHunCfW).",
 		external_link: "https://www.gweiface.com/",
 		discord: "httpsL//www.google.com",
 		discord_link: "https://www.google.com",
